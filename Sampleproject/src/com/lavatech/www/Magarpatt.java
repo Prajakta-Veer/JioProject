bec3c7f00e921fe76f335e149651c1213cd4a628
Prajakta-Veer <prajaktabhargude28@gmail.com>
2024-08-28 22:59:09 +0530

    Collection Framework Method Projects added

---
+++ Sampleproject/src/com/lavatech/www/Magarpatt.java
@@ -0,0 +1,23 @@
+package com.lavatech.www;
+
+public class TestClass1{
+
+	public static void main(String[] args) {
+		String star("*****");
+		String name1 = "Sham Kumar";
+		int age1=56;
+		String skill1="C++";
+		
+		String name2 = "Seeta";
+		int age2=28;
+		String skil2 ="C";
+		System.out.println("Details of student :1");
+		System.out.println(star);
+System.out.println("Student name: " +name1);
+System.out.println("Student age:  " +Age1);
+System.out.println("Student skill: " +skill1);
+	
+
+	}
+
+}
